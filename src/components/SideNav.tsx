a461954ca9cacf5ea1244fe30ac5a3c1fee5bba0
nity43am <nityamsahu3721@gmail.com>
2026-09-12 11:19:38 +0530

    feat: integrate Aniket3125/rc_club_event_page

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -10,7 +10,7 @@ export default function SideNav() {
       <nav className={`${styles.navStrip} ${isOpen ? styles.open : ''}`} aria-hidden={!isOpen}>
         <ul className={styles.navList}>
           <li style={{ '--i': 1 } as React.CSSProperties}>
-            <a href="/events" className={styles.navItem} onClick={() => setIsOpen(false)}>
+            <a href="/events/index.html" className={styles.navItem} onClick={() => setIsOpen(false)}>
               EVENTS
             </a>
           </li>
